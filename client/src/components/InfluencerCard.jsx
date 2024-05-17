e78c4418d447304d15b4c83e20a4fb653af7e790
Aaqib Imran <aaqibimran0@gmail.com>
2024-05-17 16:18:37 +0500

    image fixed

--- client/src/components/InfluencerCard.jsx
+++ client/src/components/InfluencerCard.jsx
@@ -7,7 +7,7 @@ const InfluencerCard = ({item}) => {
     <div key={item.id}>
     <div className="wrap ">
       <img
-        className=" rounded-t-lg "
+        className=" rounded-t-lg min-w-full "
         src={item.profilePic? (item.profilePic.includes("cloudinary")? item.profilePic : image ): image}
         alt={item.fullName}
       />
